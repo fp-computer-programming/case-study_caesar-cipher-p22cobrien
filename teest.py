4654d0ba0a7eee5821cf46a946098ef3ce0d25dd
p22cobrien <p22cobrien@fairfieldprep.org>
2022-04-27 11:14:20 -0400

    aaa

--- teest.py
+++ teest.py
@@ -1,3 +1,6 @@
+from array import array
+
+
 def green(n):
     num = n ** 2
     numlen = len(str(num))
@@ -21,4 +24,16 @@ def dont_give_me_five(start, end):
         
     return counter
 
-print(dont_give_me_five(1,9))
+#print(dont_give_me_five(1,9))
+
+
+ #return sum('5' not in str(i) for i in range(start, end + 1))
+#moving zeroes to the end
+def move_zeros(array):
+    for index, value in enumerate(array):
+        if value == 0:
+            array.append(value)
+            del array[index]
+    return array
+
+print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
